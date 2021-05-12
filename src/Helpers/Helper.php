338d7040eadfece32ffade405e3a35f40152adb2
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2021-05-12 12:20:33 +0300

    added some shapes

---
+++ src/Helpers/Helper.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @return string
+ * Current version number of library.
+ *
+ * NOTE: In the development environment you must return current version without "-dev" suffix.
+ */
+function tagtest_version()
+{
+	return "v1.1.1";
+}
+
+/***
+ * @param $a
+ * @param $b
+ * @return mixed
+ *
+ * New function definition in v1.1.0
+ */
+function tagtest_total($a, $b)
+{
+	return $a + $b;
+}
+
