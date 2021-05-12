c71a3d9f1a836f3ca64a3eeac80811bc64f7c4ed
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2021-05-12 11:20:30 +0300

    added version function

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-echo "test";
+/**
+ * @return string
+ * In the development environment you must return current version without "-dev" suffix.
+ */
+function current_version()
+{
+	return "v1.0.0";
+}
 
+echo "current version: " . current_version() . PHP_EOL;
